53a93f8b6add7c4b3798a3d0f7f713875c582e26
unknown <ahmed.asoliman1@gmail.com>
2022-08-08 08:44:30 +0200

    upload video & img to firebase storag

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
+
+// import firebase upload files functions...
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 // import icons from mui5 library...
 import CloseIcon from '@mui/icons-material/Close';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
+import app from '../firebase';
 
 
 const Container = styled.div`
@@ -193,7 +197,67 @@ const FooterTextSecond = styled.span`
 `;
 
 export default function Upload({ setOpen }) {
-    const [isClicked, setIsClicked] = useState(false)
+
+    const [isClicked, setIsClicked] = useState(false);
+    const [video, setVideo] = useState(undefined);
+    const [img, setImg] = useState(undefined);
+    const [videoProgress, setVideoProgress] = useState(0);
+    const [imgProgress, setImgProgress] = useState(0);
+    const [inputs, setInputs] = useState({});
+    const [tags, setTags] = useState([]);
+
+    const handleChange = (e) => {
+        setInputs((previous) => {
+            return { ...previous, [e.target.name]: e.target.value };
+        })
+    }
+
+    const handleTags = (e) => {
+        setTags(e.target.value.split(','));
+    }
+
+
+    // Upload file to firebase storage function...
+    const uploadFile =  (file, urlType) => {
+        const storage = getStorage(app);
+        const uniqueFileName = new Date().getTime() + file.name; // for unique file name to prevent conflic samiler file names...
+        const storageRef = ref(storage, uniqueFileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                urlType === 'imgUrl' ? setImgProgress(progress) : setVideoProgress(progress);
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                    default:
+                        break;
+                }
+            },
+            (error) => {
+
+            },
+            () => {
+                // Upload completed successfully, now we can get the download URL
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setInputs((previous) => {
+                        return { ...previous, [urlType]: downloadURL };
+                    });
+                });
+              }
+        );
+    }
+
+    useEffect(() => { video && uploadFile(video , "videoUrl") }, [video])
+    useEffect(() => { img && uploadFile(img, "imgUrl") }, [img])
     return (
         <Container>
             <Wrapper>
@@ -202,34 +266,39 @@ export default function Upload({ setOpen }) {
                 <HorizontalLine />
 
                 <UploadFileIcon>
-                    <Circle><WrapperIcons><FileUploadIcon style={{ color: "rgb(0 0 0 / 40%)", fontSize: '60px' }} /></WrapperIcons></Circle>
+                    <Circle>
+                        <WrapperIcons>
+                            <FileUploadIcon style={{ color: "rgb(0 0 0 / 40%)", fontSize: '60px' }} />
+                        </WrapperIcons>
+                    </Circle>
                     <SelectedFileText>Drag and drop video files to upload</SelectedFileText>
                     <SelectedFileDetails>Your videos will be private until you publish them.</SelectedFileDetails>
-                    <Button onClick={() => setIsClicked(true)}>SELECTED FILES
-                        <input type="file" style={{ display: 'none' }} accept="video/*" /></Button>
+                    {videoProgress>0? ("Uploading Video:" + videoProgress + "%"):(<Button onClick={() => setIsClicked(true)}>SELECTED FILES
+                        <input type="file" style={{ display: 'none' }} accept="video/*" onChange={(e) => setVideo(e.target.files[0])} />
+                    </Button>)}
                 </UploadFileIcon>
                 {isClicked && <VideoDetailsWrapper>
                     <InputsContainer>
-                        <Input type="text" placeholder="Video Title" />
-                        <Input type="text" placeholder='Video Description' />
-                        <Input type="text" placeholder='Separate tags with commas' />
+                        <Input type="text" placeholder="Video Title" onChange={handleChange} name="title"/>
+                        <Input type="text" placeholder='Video Description' onChange={handleChange} name="description"/>
+                        <Input type="text" placeholder='Separate tags with commas' onChange={handleTags} />
                     </InputsContainer>
-                    <UploadFileImage>
+                    {imgProgress>0? ("Uploading Image:"+ imgProgress + "%"):(<UploadFileImage>
                         <Button type="">
                             SELECT IMAGE
-                            <input type="file" style={{ display: 'none' }} accept="image/*" />
+                            <input type="file" style={{ display: 'none' }} accept="image/*" onChange={(e) => setImg(e.target.files[0])} />
                         </Button>
-                    </UploadFileImage>
+                    </UploadFileImage>)}
                     <SubmitFiles>
                         <UploadButton>Upload All files</UploadButton>
                     </SubmitFiles>
                 </VideoDetailsWrapper>}
-                {!isClicked && <><FooterText>
+                {!isClicked && <> <FooterText>
                     By submitting your videos to Soliman YouTube Clone, you acknowledge that you agree to <span style={{ color: 'blue', marginLeft: "5px" }}>Terms of Service</span> and <span style={{ color: 'blue', marginLeft: "5px" }}>Community Guidelines</span>.
                 </FooterText>
-                <FooterTextSecond>
-                    Please be sure not to violate others' copyright or privacy rights.<span style={{ color: 'blue', marginLeft: "5px" }}> Learn more</span>
-                </FooterTextSecond> </>}
+                    <FooterTextSecond>
+                        Please be sure not to violate others' copyright or privacy rights.<span style={{ color: 'blue', marginLeft: "5px" }}> Learn more</span>
+                    </FooterTextSecond> </>}
             </Wrapper>
         </Container>
     )
